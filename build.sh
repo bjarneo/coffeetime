bbabd2024a1f6b9ddbd08ab6119d7b11106b9a58
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-02-07 21:02:48 +0100

    Remove folder as executable

--- build.sh
+++ build.sh
@@ -6,14 +6,13 @@
 rm -rf build/
 
 # Linux
-env GOOS=linux GOARCH=arm go build -o build/cli-linux-arm -v cli.go  
-env GOOS=linux GOARCH=386 go build -o build/cli-linux-386 -v cli.go  
-env GOOS=linux GOARCH=386 go build -o build/cli-linux-amd64 -v cli.go  
+env GOOS=linux GOARCH=arm go build -o build/cli-linux-arm -v cli.go
+env GOOS=linux GOARCH=386 go build -o build/cli-linux-386 -v cli.go
+env GOOS=linux GOARCH=386 go build -o build/cli-linux-amd64 -v cli.go
 
 # macos
-env GOOS=darwin GOARCH=amd64 go build -o build/cli-darwin -v cli.go 
+env GOOS=darwin GOARCH=amd64 go build -o build/cli-darwin -v cli.go
 
 # windows
-env GOOS=windows GOARCH=amd64 go build -o build/cli-windows-amd64.exe -v cli.go 
+env GOOS=windows GOARCH=amd64 go build -o build/cli-windows-amd64.exe -v cli.go
 
-chmod +x build/
