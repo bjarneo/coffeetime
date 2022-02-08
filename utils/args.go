d23a42a2a21169ffa45c05bf3a82b33222ac1908
bjarneo <bjarne.oeverli@gmail.com>
2022-02-08 19:17:56 +0100

    refactor: Args function should return a struct with the flags
    Signed-off-by: bjarneo <bjarne.oeverli@gmail.com>

--- utils/args.go
+++ utils/args.go
@@ -4,12 +4,20 @@ import (
 	"flag"
 )
 
-func Args() (string, int, int) {
-	color := flag.String("color", "lightYellow", "the clock color")
-	interval := flag.Int("interval", 120, "The interval in minutes between coffee breaks")
-	duration := flag.Int("duration", 5, "The duration in minutes of the coffee break")
+type arguments struct {
+	Color    *string
+	Interval *int
+	Duration *int
+}
+
+func Args() arguments {
+	flags := arguments{
+		Color:    flag.String("color", "lightYellow", "the clock color"),
+		Interval: flag.Int("interval", 120, "The interval in minutes between coffee breaks"),
+		Duration: flag.Int("duration", 5, "The duration in minutes of the coffee break"),
+	}
 
 	flag.Parse()
 
-	return *color, *interval, *duration
+	return flags
 }
